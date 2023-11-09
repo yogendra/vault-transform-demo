45a7af53a015589cafb3c57888ff7e080a456605
Yogendra Rampuria - Yogi <yogendrarampuria@gmail.com>
2023-11-10 00:52:18 +0800

    Added lombok based code gen annotations

--- demo-api/src/main/java/com/hashicorp/transformdemo/entity/User.java
+++ demo-api/src/main/java/com/hashicorp/transformdemo/entity/User.java
@@ -6,10 +6,18 @@ import jakarta.persistence.Entity;
 import jakarta.persistence.Id;
 import jakarta.persistence.Table;
 import java.io.Serializable;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
 
 @Entity
 @Table(name = "users_tokenization")
 @JsonIgnoreProperties({"hibernateLazyInitializer", "handler"})
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
 public class User implements Serializable {
 
     @Id
@@ -19,52 +27,4 @@ public class User implements Serializable {
     String email;
     String creditcard;
     String flag;
-
-    public String getId() {
-        return id;
-    }
-
-    public void setId(String id) {
-        this.id = id;
-    }
-
-    public String getUsername() {
-        return username;
-    }
-
-    public void setUsername(String username) {
-        this.username = username;
-    }
-
-    public String getPassword() {
-        return password;
-    }
-
-    public void setPassword(String password) {
-        this.password = password;
-    }
-
-    public String getEmail() {
-        return email;
-    }
-
-    public void setEmail(String email) {
-        this.email = email;
-    }
-
-    public String getCreditcard() {
-        return creditcard;
-    }
-
-    public void setCreditcard(String creditcard) {
-        this.creditcard = creditcard;
-    }
-
-    public String getFlag() {
-        return flag;
-    }
-
-    public void setFlag(String flag) {
-        this.flag = flag;
-    }
 }
