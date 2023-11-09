e2df488203df395dfd3d1d04e0530310549d01b0
Yogendra Rampuria - Yogi <yogendrarampuria@gmail.com>
2023-11-10 00:57:21 +0800

    Use lombok

--- demo-ui/src/main/java/com/hashicorp/transformdemo/model/User.java
+++ demo-ui/src/main/java/com/hashicorp/transformdemo/model/User.java
@@ -1,62 +1,23 @@
 package com.hashicorp.transformdemo.model;
 
 import java.io.Serializable;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import lombok.Value;
 
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
 public class User implements Serializable {
 
-    String id;
-    String username;
-    String password;
-    String email;
-    String creditcard;
-    String flag;
-
-    public String getId() {
-        return id;
-    }
-
-    public void setId(String id) {
-        this.id = id;
-    }
-
-    public String getUsername() {
-        return username;
-    }
-
-    public void setUsername(String username) {
-        this.username = username;
-    }
-
-    public String getPassword() {
-        return password;
-    }
-
-    public void setPassword(String password) {
-        this.password = password;
-    }
-
-    public String getEmail() {
-        return email;
-    }
-
-    public void setEmail(String email) {
-        this.email = email;
-    }
-
-    public String getCreditcard() {
-        return creditcard;
-    }
-
-    public void setCreditcard(String creditcard) {
-        this.creditcard = creditcard;
-    }
-
-    public String getFlag() {
-        return flag;
-    }
-
-    public void setFlag(String flag) {
-        this.flag = flag;
-    }
+  private String id;
+  private String username;
+  private String password;
+  private String email;
+  private String creditcard;
+  private String flag;
 
 }
