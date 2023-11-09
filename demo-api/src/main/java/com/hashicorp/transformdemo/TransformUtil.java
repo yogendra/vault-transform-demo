50c19907e134d6641c227e7e2aaf519eaf7aa572
Yogendra Rampuria - Yogi <yogendrarampuria@gmail.com>
2023-11-10 00:54:24 +0800

    Replaced JSON package and cleaned up code

--- demo-api/src/main/java/com/hashicorp/transformdemo/TransformUtil.java
+++ demo-api/src/main/java/com/hashicorp/transformdemo/TransformUtil.java
@@ -1,84 +1,42 @@
 package com.hashicorp.transformdemo;
 
-import org.json.JSONObject;
-import org.springframework.context.annotation.Bean;
-import org.springframework.http.HttpEntity;
-import org.springframework.http.HttpHeaders;
-import org.springframework.http.MediaType;
-import org.springframework.http.ResponseEntity;
-import org.springframework.web.client.RestTemplate;
 
-import java.util.Collections;
 import java.util.LinkedHashMap;
 import java.util.Map;
+import org.springframework.stereotype.Component;
 
+@Component
 public class TransformUtil {
 
-    @Bean
-    public RestTemplate restTemplate() {
-        return new RestTemplate();
-    }
+  private final VaultApiClient vaultApiClient;
 
-    private String vaultHost = System.getenv("SPRING_CLOUD_VAULT_HOST") != null ? System.getenv("SPRING_CLOUD_VAULT_HOST") : "localhost";
+  public TransformUtil( VaultApiClient vaultApiClient) {
+    this.vaultApiClient = vaultApiClient;
+  }
 
-    public String encode(String value, String transformationName) {
+  public String encode(String value, String transformationName) {
+    Map<String, Object> input = new LinkedHashMap<>();
+    input.put("transformation", transformationName);
+    input.put("value", value);
+    var data = vaultApiClient.postToVault("/v1/transform/encode/payments", input);
+    return data.getEncodedValue();
 
-        HttpHeaders headers = new HttpHeaders();
-        headers.setContentType(MediaType.APPLICATION_JSON);
-        headers.setAccept(Collections.singletonList(MediaType.APPLICATION_JSON));
+  }
 
-        Map<String, Object> input = new LinkedHashMap<>();
-        input.put("transformation", transformationName);
-        input.put("value", value);
+  public String decode(String value, String transformationName) {
+    var input = new LinkedHashMap<String, Object>();
+    input.put("transformation", transformationName);
+    input.put("value", value);
+    var data = vaultApiClient.postToVault("/v1/transform/decode/payments", input);
+    return data.getDecodedValue();
+  }
 
-        System.out.println(input.toString());
-
-        HttpEntity<Map<String, Object>> entity = new HttpEntity<>(input, headers);
-
-        ResponseEntity<String> response = this.restTemplate().postForEntity(String.format("http://%s:8100/v1/transform/encode/payments", vaultHost), entity, String.class);
-        JSONObject jsonObject = new JSONObject(response.getBody());
-
-        return jsonObject.getJSONObject("data").getString("encoded_value");
-    }
-
-    public String decode(String value, String transformationName) {
-
-        HttpHeaders headers = new HttpHeaders();
-        headers.setContentType(MediaType.APPLICATION_JSON);
-        headers.setAccept(Collections.singletonList(MediaType.APPLICATION_JSON));
-
-        Map<String, Object> input = new LinkedHashMap<>();
-        input.put("transformation", transformationName);
-        input.put("value", value);
-
-        System.out.println(input.toString());
-
-        HttpEntity<Map<String, Object>> entity = new HttpEntity<>(input, headers);
-
-        ResponseEntity<String> response = this.restTemplate().postForEntity(String.format("http://%s:8100/v1/transform/decode/payments", vaultHost), entity, String.class);
-        JSONObject jsonObject = new JSONObject(response.getBody());
-
-        return jsonObject.getJSONObject("data").getString("decoded_value");
-    }
-
-    public String masking(String value, String transformationName) {
-
-        HttpHeaders headers = new HttpHeaders();
-        headers.setContentType(MediaType.APPLICATION_JSON);
-        headers.setAccept(Collections.singletonList(MediaType.APPLICATION_JSON));
-
-        Map<String, Object> input = new LinkedHashMap<>();
-        input.put("transformation", transformationName);
-        input.put("value", value);
-
-        System.out.println(input.toString());
-
-        HttpEntity<Map<String, Object>> entity = new HttpEntity<>(input, headers);
-
-        ResponseEntity<String> response = this.restTemplate().postForEntity(String.format("http://%s:8100/v1/transform/encode/payments", vaultHost), entity, String.class);
-        JSONObject jsonObject = new JSONObject(response.getBody());
-
-        return jsonObject.getJSONObject("data").getString("encoded_value");
-    }
-
+  public String masking(String value, String transformationName) {
+    var input = new LinkedHashMap<String, Object>();
+    input.put("transformation", transformationName);
+    input.put("value", value);
+    var data = vaultApiClient.postToVault("/v1/transform/encode/payments", input);
+    return data.getEncodedValue();
+  }
 }
+
--- demo-api/src/main/java/com/hashicorp/transformdemo/TransitUtil.java
+++ demo-api/src/main/java/com/hashicorp/transformdemo/TransitUtil.java
@@ -1,56 +1,40 @@
 package com.hashicorp.transformdemo;
 
-import org.json.JSONObject;
-import org.springframework.context.annotation.Bean;
-import org.springframework.http.HttpEntity;
-import org.springframework.http.HttpHeaders;
-import org.springframework.http.MediaType;
-import org.springframework.http.ResponseEntity;
-import org.springframework.web.client.RestTemplate;
+import static java.lang.String.format;
 
 import java.util.Base64;
-import java.util.Collections;
 import java.util.LinkedHashMap;
 import java.util.Map;
+import org.springframework.stereotype.Component;
 
+@Component
 public class TransitUtil {
 
-    private String vaultHost = System.getenv("SPRING_CLOUD_VAULT_HOST") != null ? System.getenv("SPRING_CLOUD_VAULT_HOST") : "localhost";
+  private final VaultApiClient vaultApiClient;
 
-    @Bean
-    public RestTemplate restTemplate() {
-        return new RestTemplate();
-    }
+  private final Config config;
 
-    public String encrypt(String plaintext) {
-        HttpHeaders headers = new HttpHeaders();
-        headers.setContentType(MediaType.APPLICATION_JSON);
-        headers.setAccept(Collections.singletonList(MediaType.APPLICATION_JSON));
+  public TransitUtil(VaultApiClient vaultApiClient, Config config) {
+    this.vaultApiClient = vaultApiClient;
+    this.config = config;
+  }
 
-        Map<String, Object> input = new LinkedHashMap<>();
-        input.put("plaintext", Base64.getEncoder().encodeToString(plaintext.getBytes()));
 
-        HttpEntity<Map<String, Object>> entity = new HttpEntity<>(input, headers);
+  public String encrypt(String plaintext) {
+    Map<String, Object> input = new LinkedHashMap<>();
+    input.put("plaintext", Base64.getEncoder()
+      .encodeToString(plaintext.getBytes()));
+    var api = format("/v1/transit/encrypt/%1$s", config.getKeyName());
+    var data = vaultApiClient.postToVault(api, input);
+    return data.getCipherText();
+  }
 
-        ResponseEntity<String> response = this.restTemplate().postForEntity(String.format("http://%s:8100/v1/transit/encrypt/my-key", vaultHost), entity, String.class);
-        JSONObject jsonObject = new JSONObject(response.getBody());
+  public String decrypt(String ciphertext) {
+    Map<String, Object> input = new LinkedHashMap<>();
+    input.put("ciphertext", ciphertext);
+    var api = format("/v1/transit/decrypt/%1$s", config.getKeyName());
+    var data = vaultApiClient.postToVault(api, input);
+    return data.getPlainText();
+  }
 
-        return jsonObject.getJSONObject("data").getString("ciphertext");
-    }
-
-    public String decrypt(String ciphertext) {
-        HttpHeaders headers = new HttpHeaders();
-        headers.setContentType(MediaType.APPLICATION_JSON);
-        headers.setAccept(Collections.singletonList(MediaType.APPLICATION_JSON));
-
-        Map<String, Object> input = new LinkedHashMap<>();
-        input.put("ciphertext", ciphertext);
-
-        HttpEntity<Map<String, Object>> entity = new HttpEntity<>(input, headers);
-
-        ResponseEntity<String> response = this.restTemplate().postForEntity(String.format("http://%s:8100/v1/transit/decrypt/my-key", vaultHost), entity, String.class);
-        JSONObject jsonObject = new JSONObject(response.getBody());
-
-        return jsonObject.getJSONObject("data").getString("plaintext");
-    }
 }
