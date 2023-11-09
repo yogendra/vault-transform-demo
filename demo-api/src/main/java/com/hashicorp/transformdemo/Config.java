50e42a3d6b2086e36755a5a2f7bc8b3cc0337355
Yogendra Rampuria - Yogi <yogendrarampuria@gmail.com>
2023-11-10 00:53:04 +0800

    Add app config

---
+++ demo-api/src/main/java/com/hashicorp/transformdemo/Config.java
@@ -0,0 +1,14 @@
+package com.hashicorp.transformdemo;
+
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+@ConfigurationProperties(prefix = "app.config")
+@Data
+@NoArgsConstructor
+public class Config {
+  private String keyName = "my-key";
+}
