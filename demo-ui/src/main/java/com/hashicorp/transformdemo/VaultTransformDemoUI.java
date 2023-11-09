6966c066c9d5b26a1e67cb516d297dd2d4d1fe2f
Yogendra Rampuria - Yogi <yogendrarampuria@gmail.com>
2023-11-10 00:59:12 +0800

    Cleanup

--- demo-ui/src/main/java/com/hashicorp/transformdemo/VaultTransformDemoUI.java
+++ demo-ui/src/main/java/com/hashicorp/transformdemo/VaultTransformDemoUI.java
@@ -6,9 +6,10 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class VaultTransformDemoUI {
 
-    public static void main(String[] args) {
-        SpringApplication.run(VaultTransformDemoUI.class, args);
-    }
+
+  public static void main(String[] args) {
+    SpringApplication.run(VaultTransformDemoUI.class, args);
+  }
 
 }
 
