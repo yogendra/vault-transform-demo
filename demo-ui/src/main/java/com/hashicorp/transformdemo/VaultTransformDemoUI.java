2d8bdce9c7327d5438e338feb68813b512f58dd9
Yogendra Rampuria - Yogi <yogendrarampuria@gmail.com>
2023-12-02 07:29:55 +0000

    removing space to trigger a build for UI

--- demo-ui/src/main/java/com/hashicorp/transformdemo/VaultTransformDemoUI.java
+++ demo-ui/src/main/java/com/hashicorp/transformdemo/VaultTransformDemoUI.java
@@ -6,7 +6,6 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class VaultTransformDemoUI {
 
-
   public static void main(String[] args) {
     SpringApplication.run(VaultTransformDemoUI.class, args);
   }
